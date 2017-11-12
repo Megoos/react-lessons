cb19020058285bfdba7fcf3254f8be8899e1fec1
Artem Samofalov <dex157@gmail.com>
2017-11-12 14:41:36 +0500

    remove test duplicate

--- src/__tests__/CardForm.test.js
+++ src/__tests__/CardForm.test.js
@@ -14,59 +14,25 @@ describe('Компонент CardForm', () => {
         expect(wrapper.find('.left-time')).toHaveLength(1);
       });
       it('.card-form input[name="cardNumber"]', () => {
-        expect(
-          wrapper.find('.card-form input[name="cardNumber"]')
-        ).toHaveLength(1);
+        expect(wrapper.find('.card-form input[name="cardNumber"]')).toHaveLength(1);
       });
 
       it('onChange input[name="cardNumber"] должен вызывать handleChangeForm', () => {
         const onChangeFormMock = jest.fn();
-        const wrapper = shallow(
-          <CardForm
-            onChangeTimeOver={jest.fn()}
-            onChangeForm={onChangeFormMock}
-          />
-        );
+        const wrapper = shallow(<CardForm onChangeTimeOver={jest.fn()} onChangeForm={onChangeFormMock} />);
 
         wrapper.find('input[name="cardNumber"]').simulate('change', {
           target: {value: 'test-value', name: 'test-name'}
         });
 
         expect(onChangeFormMock).toHaveBeenCalledTimes(1);
-        expect(onChangeFormMock).toHaveBeenLastCalledWith(
-          'test-name',
-          'test-value'
-        );
-      });
-      it('onChange input[name="cardNumber"] должен вызывать handleChangeForm', () => {
-        const onChangeFormMock = jest.fn();
-        const wrapper = shallow(
-          <CardForm
-            onChangeTimeOver={jest.fn()}
-            onChangeForm={onChangeFormMock}
-          />
-        );
-
-        wrapper.find('input[name="cardNumber"]').simulate('change', {
-          target: {value: 'test-value', name: 'test-name'}
-        });
-
-        expect(onChangeFormMock).toHaveBeenCalledTimes(1);
-        expect(onChangeFormMock).toHaveBeenLastCalledWith(
-          'test-name',
-          'test-value'
-        );
+        expect(onChangeFormMock).toHaveBeenLastCalledWith('test-name', 'test-value');
       });
     });
 
     describe('Методы класса', () => {
       const onChangeFormMock = jest.fn();
-      const wrapper = mount(
-        <CardForm
-          onChangeTimeOver={jest.fn()}
-          onChangeForm={onChangeFormMock}
-        />
-      );
+      const wrapper = mount(<CardForm onChangeTimeOver={jest.fn()} onChangeForm={onChangeFormMock} />);
 
       describe('handleChangeForm', () => {
         it('Присутствует', () => {
@@ -77,10 +43,7 @@ describe('Компонент CardForm', () => {
             target: {name: 'test-name', value: 'test-value'}
           });
           expect(onChangeFormMock).toHaveBeenCalledTimes(1);
-          expect(onChangeFormMock).toHaveBeenLastCalledWith(
-            'test-name',
-            'test-value'
-          );
+          expect(onChangeFormMock).toHaveBeenLastCalledWith('test-name', 'test-value');
         });
       });
       describe('componentWillUnmount', () => {
