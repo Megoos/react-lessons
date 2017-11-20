e72a71c4181cf7067c1b137e3f67bc3638f6edf6
Artem Samofalov <dex157@gmail.com>
2017-11-20 12:35:34 +0500

    update tests

--- src/reducers/__tests__/market.test.js
+++ src/reducers/__tests__/market.test.js
@@ -1,15 +1,26 @@
-import {CREATE_ORDER, MOVE_ORDER_TO_FARM} from '../../actions/marketTypes';
+import {
+  CREATE_ORDER,
+  MOVE_ORDER_TO_FARM
+} from '../../actions/marketTypes';
 import market from '../market';
 
 describe('reducer market', () => {
   it('экшен с типом CREATE_ORDER добавляет action.payload к orders', () => {
-    const next = market({}, {type: CREATE_ORDER, payload: {id: 1}});
+    const next = market(undefined, {
+      type: CREATE_ORDER,
+      payload: {id: 1}
+    });
 
-    expect([{id: 1}]).toEqual(expect.arrayContaining(next.orders));
+    expect([{id: 1}]).toEqual(
+      expect.arrayContaining(next.orders)
+    );
   });
 
   it('экшен с типом MOVE_ORDER_TO_FARM удаляет из orders по action.payload.id', () => {
-    const next = market({orders: [{id: 1}]}, {type: MOVE_ORDER_TO_FARM, payload: {id: 1}});
+    const next = market(
+      {orders: [{id: 1}]},
+      {type: MOVE_ORDER_TO_FARM, payload: {id: 1}}
+    );
 
     expect(next.orders).toEqual([]);
   });
--- src/store.js
+++ src/store.js
@@ -1,13 +1,5 @@
-import {createStore, compose} from 'redux';
+import {createStore} from 'redux';
 import rootReducer from './reducers';
 
 export default initialState =>
-  createStore(
-    rootReducer,
-    initialState,
-    compose(
-      window.devToolsExtension
-        ? window.__REDUX_DEVTOOLS_EXTENSION__()
-        : f => f
-    )
-  );
+  createStore(rootReducer, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
