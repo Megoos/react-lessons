541d8b744161070c28174ef53f404b1ef2d95290
Megoos <meguskov@gmail.com>
2017-11-21 17:39:41 +0300

    problem with Auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Link, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
-import {addListener, removeListener, isAuthorized} from './AuthorizeApi';
+import { addListener, removeListener, isAuthorized } from './AuthorizeApi';
+
+import Auth from './Auth';
+import Private from './Private';
+import Public from './Public';
+import Home from './Home';
 
 class App extends Component {
   state = {
@@ -16,11 +22,46 @@ class App extends Component {
   }
 
   handleAuthorize = isAuthorized => {
-    this.setState({isAuthorized});
+    this.setState({ isAuthorized });
   };
 
   render() {
-    return null;
+    const { isAuthorized } = this.state;
+
+    return (
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/auth">Войти</Link>
+            </li>
+            <li>
+              <Link to="/private">Секретная страница</Link>
+            </li>
+            <li>
+              <Link to="/public">Публичная страница</Link>
+            </li>
+            <li>
+              <Link to="/">Главная</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          {!isAuthorized ? (
+            <Redirect from="/private" to="/auth" />
+          ) : (
+            <Route path="/private" component={Private} />
+          )}
+          <Route
+            path="/auth"
+            render={props => <Auth isAuthorized={isAuthorized} {...props} />}
+          />
+          <Route path="/public" component={Public} />
+          <Redirect from="*" to="/" />
+        </Switch>
+      </div>
+    );
   }
 }
 
--- src/Auth.js
+++ src/Auth.js
@@ -1,8 +1,54 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { authorizeUser } from './AuthorizeApi';
+import { Redirect } from 'react-router-dom';
 
 class Auth extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state.isAuthorized = this.props.isAuthorized;
+  }
+
+  state = {
+    email: '',
+    password: '',
+    error: false
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = () => {
+    const { email, password } = this.state;
+    this.setState({ error: !authorizeUser(email, password) });
+  };
+
   render() {
-    return null;
+    const { isAuthorized, error } = this.state;
+    return isAuthorized ? (
+      <Redirect from="/auth" to="/" />
+    ) : (
+      <form action="">
+        <input
+          name="email"
+          type="text"
+          placeholder="email"
+          onChange={this.handleChange}
+        />
+        <input
+          name="password"
+          type="text"
+          placeholder="password"
+          onChange={this.handleChange}
+        />
+
+        <button onClick={this.handleSubmit}>Submit</button>
+
+        {error && <p className="error">Email or password is wrong</p>}
+      </form>
+    );
   }
 }
 
